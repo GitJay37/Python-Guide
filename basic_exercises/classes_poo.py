41e01d0bbe5a072a0bbf7ba9c2a7d187b4352e21
GitJay37 <jeisondejesus@hotmail.es>
2019-10-26 13:19:45 -0500

    classes poo py file

---
+++ basic_exercises/classes_poo.py
@@ -0,0 +1,4 @@
+class TestClass: # Definición de una clase
+    print("Hola Jeison") # Declaración, oreden, sentencia o bloque de codigo a ejecutarse
+
+pyhton_course = TestClass() # Objeto intanciando de la clase TestClass :) 
